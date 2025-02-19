2edcbfba5c1e650bde72c39b2229e9be83ab619f
Nassim HADDAD <nassim.haddad.pro@gmail.com>
2025-02-19 17:34:47 +0100

    feat: new font + top menu

--- themes/classic/assets/js/custom.js
+++ themes/classic/assets/js/custom.js
@@ -5,3 +5,82 @@ function onRecaptchaSuccess() {
     // Enable your submit button
     document.getElementById("submitMessage").disabled = false;
 }
+
+$(document).ready(function () {
+    const searchWidget = document.getElementById("search_widget");
+
+    searchWidget.addEventListener("focusin", function () {
+        searchWidget.classList.toggle("search_widget_active");
+    });
+
+    searchWidget.addEventListener("focusout", function () {
+        searchWidget.classList.toggle("search_widget_active");
+    });
+
+    function hideAllPanels() {
+        document
+            .querySelectorAll(
+                "#_desktop_top_menu>.top-menu>.category>.dropdown-item"
+            )
+            .forEach((button) => {
+                button.classList.remove("top-menu-active-category");
+            });
+
+        document
+            .querySelectorAll(
+                "#_desktop_top_menu>.top-menu>.category>.sub-menu"
+            )
+            .forEach((panel) => {
+                panel.classList.remove("collapse");
+                panel.classList.remove("sub-menu-active");
+            });
+    }
+
+    document
+        .querySelectorAll(
+            "#_desktop_top_menu>.top-menu>.category>.dropdown-item"
+        )
+        .forEach((button) => {
+            button.addEventListener("click", function () {
+                // Remove active class from all buttons
+                document.querySelectorAll(".dropdown-item").forEach((btn) => {
+                    btn.classList.remove("top-menu-active-category");
+                });
+
+                // Add active class to clicked button
+                this.classList.add("top-menu-active-category");
+
+                // Hide all content panels
+
+                // Show target panel
+                const targetPanel = document.getElementById(
+                    this.dataset.target
+                );
+                console.log(targetPanel);
+                // if sub menu is open, close it
+                let arrow = this.querySelector(".top-menu-arrow");
+                if (targetPanel.classList.contains("sub-menu-active")) {
+                    targetPanel.classList.remove("sub-menu-active");
+                    targetPanel.classList.add("collapse");
+                    arrow.classList.remove("rotate-180");
+
+                    console.log("close");
+                } else {
+                    console.log("open");
+                    hideAllPanels();
+                    targetPanel.classList.remove("collapse");
+                    targetPanel.classList.add("sub-menu-active");
+                    // rotate the arrow
+                    arrow.classList.add("rotate-180");
+                }
+            });
+        });
+
+    document.getElementById("wrapper").addEventListener("click", function (e) {
+        // if (e.target.id !== "wrapper") {
+        console.log("wrapper focus");
+
+        hideAllPanels();
+        // }
+    });
+});
--- themes/classic/modules/ps_mainmenu/ps_mainmenu.tpl
+++ themes/classic/modules/ps_mainmenu/ps_mainmenu.tpl
@@ -5,11 +5,13 @@
       {foreach from=$nodes item=node}
         <li class="{$node.type}{if $node.current} current {/if}" id="{$node.page_identifier}">
           {assign var=_counter value=$_counter+1}
-          <a class="{if $depth >= 0}dropdown-item{/if}{if $depth === 1} dropdown-submenu{/if}" href="{$node.url}"
-            data-depth="{$depth}" {if $node.open_in_new_window} target="_blank" {/if}>
+          {assign var=_expand_id value=10|mt_rand:100000}
+          <a class="{if $depth >= 0}dropdown-item{/if}{if $depth === 1} dropdown-submenu{/if}"
+            {if $depth >= 0}data-target="top_sub_menu_{$_expand_id}" {/if}
+            {if $depth > 0 || $node.page_identifier == 'lnk-tapis-sur-mesure'}href="{$node.url}" {/if} data-depth="{$depth}"
+            {if $node.open_in_new_window} target="_blank" {/if}>
             {if $node.children|count}
               {* Cannot use page identifier as we can have the same page several times *}
-              {assign var=_expand_id value=10|mt_rand:100000}
               <span class="float-xs-right hidden-md-up">
                 <span data-target="#top_sub_menu_{$_expand_id}" data-toggle="collapse" class="navbar-toggler collapse-icons">
                   <i class="material-icons add">&#xE313;</i>
@@ -18,6 +20,15 @@
               </span>
             {/if}
             {$node.label}
+            {if $node.children|count && $depth == 0}
+              <svg class="svg-inline--fa fa-chevron-down h-4 w-4 top-menu-arrow" aria-hidden="true" focusable="false"
+                data-prefix="fas" data-icon="chevron-down" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"
+                data-fa-i2svg="">
+                <path fill="currentColor"
+                  d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z">
+                </path>
+              </svg>
+            {/if}
           </a>
           {if $node.children|count}
             <div {if $depth === 0} class="popover sub-menu js-sub-menu collapse" {else} class="" {/if}
